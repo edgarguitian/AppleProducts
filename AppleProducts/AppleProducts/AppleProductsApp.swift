80ed2eaaf641c0699e16bf959c8d1e85775c698d
Edgar <edguitian@imagine800.es>
2024-06-20 12:32:38 +0200

    [Feature] Project Creation

---
+++ AppleProducts/AppleProducts/AppleProductsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleProductsApp.swift
+//  AppleProducts
+//
+//  Created by Edgar Guitian Rey on 20/6/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleProductsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
